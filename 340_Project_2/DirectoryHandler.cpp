a6f92cebc5bd5315a9c18dc332e50584e0046ac6
Bryan <bryanlee56098@gmail.com>
2024-03-13 21:03:46 -0700

    Added DirectoryHandler.h

---
+++ 340_Project_2/DirectoryHandler.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include "Employee.h"
+#include "Department.h"
+#include "Floor.h"
+using namespace std;
+
---
+++ 340_Project_2/DirectoryHandler.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<iostream>
+#include<map>
+#include<string>
+#include "Employee.h"
+#include "Department.h"
+#include "Floor.h"
+using namespace std;
+
+class DirectoryHandler {
+private:
+	map<int, Employee*> employeesMap;
+	map<string, Department*> departmentsMap;
+	map<int, Floor*> floorsMap;
+public:
+	//Add a new Employee record
+	void addEmployee(Employee* newEmployee);
+
+	//Find a employee by ID number
+	Employee* findEmployee(int employeeID);
+
+	//List all employees in a given department
+	vector<Employee*>& listEmployeesInDept(string departmentName);
+
+	//List all employees on a given floor
+	vector<Employee*>& listEmployeesOnFloor(int floorNum);
+};
