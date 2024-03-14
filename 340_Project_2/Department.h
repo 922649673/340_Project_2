75923dd3e272d44c61032cfe14bd9f7191e9854c
Bryan Lee <blee37@sfsu.edu>
2024-03-13 19:57:14 -0700

    yipee

--- 340_Project_2/Department.h
+++ 340_Project_2/Department.h
@@ -1,1 +1,11 @@
 #pragma once
+#include<iostream>
+#include <vector>
+using namespace std;
+
+class Department {
+private:
+	string departmentName, employeeName;
+public:
+	std::vector<string> departmentVector {employeeName*};
+};
